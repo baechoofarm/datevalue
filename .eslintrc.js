a0ecab5adddc871e551b76dde97c92d37c935cea
Byeonggeol Ha <byeonggeol98@gmail.com>
2021-11-23 16:10:57 +0900

    setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "parser": "@typescript-eslint/parser",
     "plugins": ["@typescript-eslint"],
-    "extends": ["airbnb", "airbnb/hooks", "plugin:@typescript-eslint/recommended"],
+    "extends": ["airbnb-base", "plugin:@typescript-eslint/recommended"],
     "settings": {
         "import/resolver": {
             "node": {
---
+++ test/sample.test.ts
@@ -0,0 +1,5 @@
+import {sum} from "../src";
+
+test('this is my test', () => {
+    expect(sum(1, 2)).toBe(3);
+});
