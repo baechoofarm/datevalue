84669241974d58bbe1c29d46fc50ff3d969250f6
Byeonggeol Ha <byeonggeol98@gmail.com>
2021-11-25 16:09:33 +0900

    Write tests

--- test/yearValue.test.ts
+++ test/yearValue.test.ts
@@ -80,7 +80,91 @@ test('YearValue.gtYear()', () => {
     const v3 = new YearValue(1998);
     const v4 = new YearValue(2024);
 
+    expect(YearValue.gtYear(2021, 2021)).toBeFalsy();
+    expect(YearValue.gtYear(2021, 1998)).toBeTruthy();
+    expect(YearValue.gtYear(2021, 2024)).toBeFalsy();
+
+    expect(YearValue.gtYear(v1, 2021)).toBeFalsy();
+    expect(YearValue.gtYear(v1, 1998)).toBeTruthy();
+    expect(YearValue.gtYear(v1, 2024)).toBeFalsy();
+
     expect(YearValue.gtYear(v1, v2)).toBeFalsy();
     expect(YearValue.gtYear(v1, v3)).toBeTruthy();
     expect(YearValue.gtYear(v1, v4)).toBeFalsy();
+});
+
+test('YearValue.gteYear()', () => {
+    const v1 = new YearValue(2021);
+    const v2 = new YearValue(2021);
+    const v3 = new YearValue(1998);
+    const v4 = new YearValue(2024);
+
+    expect(YearValue.gteYear(2021, 2021)).toBeTruthy();
+    expect(YearValue.gteYear(2021, 1998)).toBeTruthy();
+    expect(YearValue.gteYear(2021, 2024)).toBeFalsy();
+
+    expect(YearValue.gteYear(v1, 2021)).toBeTruthy();
+    expect(YearValue.gteYear(v1, 1998)).toBeTruthy();
+    expect(YearValue.gteYear(v1, 2024)).toBeFalsy();
+
+    expect(YearValue.gteYear(v1, v2)).toBeTruthy();
+    expect(YearValue.gteYear(v1, v3)).toBeTruthy();
+    expect(YearValue.gteYear(v1, v4)).toBeFalsy();
+});
+
+test('YearValue.ltYear()', () => {
+    const v1 = new YearValue(2021);
+    const v2 = new YearValue(2021);
+    const v3 = new YearValue(1998);
+    const v4 = new YearValue(2024);
+
+    expect(YearValue.ltYear(2021, 2021)).toBeFalsy();
+    expect(YearValue.ltYear(2021, 1998)).toBeFalsy();
+    expect(YearValue.ltYear(2021, 2024)).toBeTruthy();
+
+    expect(YearValue.ltYear(v1, 2021)).toBeFalsy();
+    expect(YearValue.ltYear(v1, 1998)).toBeFalsy();
+    expect(YearValue.ltYear(v1, 2024)).toBeTruthy();
+
+    expect(YearValue.ltYear(v1, v2)).toBeFalsy();
+    expect(YearValue.ltYear(v1, v3)).toBeFalsy();
+    expect(YearValue.ltYear(v1, v4)).toBeTruthy();
+});
+
+test('YearValue.lteYear()', () => {
+    const v1 = new YearValue(2021);
+    const v2 = new YearValue(2021);
+    const v3 = new YearValue(1998);
+    const v4 = new YearValue(2024);
+
+    expect(YearValue.lteYear(2021, 2021)).toBeTruthy();
+    expect(YearValue.lteYear(2021, 1998)).toBeFalsy();
+    expect(YearValue.lteYear(2021, 2024)).toBeTruthy();
+
+    expect(YearValue.lteYear(v1, 2021)).toBeTruthy();
+    expect(YearValue.lteYear(v1, 1998)).toBeFalsy();
+    expect(YearValue.lteYear(v1, 2024)).toBeTruthy();
+
+    expect(YearValue.ltYear(v1, v2)).toBeTruthy();
+    expect(YearValue.ltYear(v1, v3)).toBeFalsy();
+    expect(YearValue.ltYear(v1, v4)).toBeTruthy();
+});
+
+test('YearValue.equalsYear()', () => {
+    const v1 = new YearValue(2021);
+    const v2 = new YearValue(2021);
+    const v3 = new YearValue(1998);
+    const v4 = new YearValue(2024);
+
+    expect(YearValue.equalsYear(2021, 2021)).toBeTruthy();
+    expect(YearValue.equalsYear(2021, 1998)).toBeFalsy();
+    expect(YearValue.equalsYear(2021, 2024)).toBeFalsy();
+
+    expect(YearValue.equalsYear(v1, 2021)).toBeTruthy();
+    expect(YearValue.equalsYear(v1, 1998)).toBeFalsy();
+    expect(YearValue.equalsYear(v1, 2024)).toBeFalsy();
+
+    expect(YearValue.equalsYear(v1, v2)).toBeTruthy();
+    expect(YearValue.equalsYear(v1, v3)).toBeFalsy();
+    expect(YearValue.equalsYear(v1, v4)).toBeFalsy();
 });
