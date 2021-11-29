4ce09d47106af89196799d20a7b47076cc3256d1
Byeonggeol Ha <byeonggeol98@gmail.com>
2021-11-29 17:57:15 +0900

    Write MonthValue tests

--- test/monthValue.test.ts
+++ test/monthValue.test.ts
@@ -78,6 +78,9 @@ test('MonthValue.set()', () => {
     v1.set(2021, 10 - 11);
     expect(v1.y).toBe(2020);
     expect(v1.m).toBe(11);
+
+    v1.set(2022);
+    expect(v1.y).toBe(2022);
 });
 
 test('MonthValue.lastDateOfMonth()', () => {
@@ -92,4 +95,44 @@ test('MonthValue.lastDateOfMonth()', () => {
     expect(v1.lastDateOfMonth).toBe(31);
     expect(v2.lastDateOfMonth).toBe(28);
     expect(v3.lastDateOfMonth).toBe(29);
+});
+
+test('MonthValue.monthDiff()', () => {
+    const v1 = new MonthValue(2021, 9);
+    const v2 = new MonthValue(2022, 2);
+
+    expect(MonthValue.monthDiff(2021, 9, 2022, 2)).toBe(5);
+    expect(MonthValue.monthDiff(2022, 2, 2021, 9)).toBe(5);
+    expect(MonthValue.monthDiff(2021, 9, 2021, 9)).toBe(0);
+
+    expect(MonthValue.monthDiff(v1, 2022, 2)).toBe(5);
+    expect(MonthValue.monthDiff(2021, 9, v2)).toBe(5);
+    expect(MonthValue.monthDiff(v1, v1)).toBe(0);
+    expect(MonthValue.monthDiff(v1, v2)).toBe(5);
+
+    // error case
+    expect(MonthValue.monthDiff(2021, 2022)).toBe(0);
+
+    expect(v1.monthDiff(2022, 2)).toBe(5);
+    expect(v2.monthDiff(2022, 2)).toBe(0);
+    expect(v1.monthDiff(v2)).toBe(5);
+});
+
+test('MonthValue.compareMonth()', () => {
+    const v1 = new MonthValue(1998, 0);
+    const v2 = new MonthValue(2021, 10);
+
+    expect(MonthValue.compareMonth(1998, 0, 2021, 10)).toBe(-1);
+    expect(MonthValue.compareMonth(2021, 10, 1998, 0)).toBe(1);
+    expect(MonthValue.compareMonth(1998, 0, 1998, 0)).toBe(0);
+
+    expect(MonthValue.compareMonth(v1, 2021, 10)).toBe(-1);
+    expect(MonthValue.compareMonth(v1, 1998, 0)).toBe(0);
+    expect(MonthValue.compareMonth(v2, 1998, 0)).toBe(1);
+    expect(MonthValue.compareMonth(1998, 0, v2)).toBe(-1);
+    expect(MonthValue.compareMonth(2021, 10, v2)).toBe(0);
+
+    expect(MonthValue.compareMonth(v1, v2)).toBe(-1);
+    expect(MonthValue.compareMonth(v2, v1)).toBe(1);
+    expect(MonthValue.compareMonth(v2, v2)).toBe(0);
 });
